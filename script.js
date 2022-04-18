c271bbc481f6adb1bbf325ca70e45131b06d9d81
Sneh Patel <snehp491@gmail.com>
2022-04-17 21:46:02 -0400

    save and get back cities from local storage

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 const openCityApiUrl = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
 
 const cities = {};
+
+let existingCities;
+
+// Prepare the website from recent history
+if (localStorage.getItem('recentCities')) {
+    existingCities = JSON.parse(localStorage.getItem('recentCities'));
+    console.log(existingCities);
+} else {
+    existingCities = new Array();
+}
+
+// Add a new city to the list of recent cities
+function addItem(id, city, region, latitude, longitude) {
+    console.log('add cities');
+    existingCities.push({id: id, city: city, region: region, latitude: latitude, longitude: longitude});
+    localStorage.setItem('recentCities', JSON.stringify(existingCities));
+}
+
+// Load a specific city details and save it in recent cities
+function loadDetail($event) {
+    const cityId = $event.target.id;
+    const city = cities[cityId];
+
+    addItem(cityId, city['city'], city['region'], city['latitude'], city['longitude']);
+}
+
+// Search for cities via API
 function searchCities() {
     const searchInput = document.getElementById('cityInput');
     const query = searchInput.value;
@@ -45,7 +72,7 @@ function searchCities() {
         resultButton.className = 'btn btn-secondary full-width mt-2';
         resultButton.textContent = results['data'][i].city + ', ' + results['data'][i].regionCode;
         resultButton.id = results['data'][i]['id'];
-
+        resultButton.addEventListener('click', loadDetail);
         item.append(resultButton);
         row.append(item);
         resultsSection.append(row);
